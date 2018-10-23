f185f96dfb7cc2d213b1878b6ec6b54072f0a574
dalurness <43400680+dalurness@users.noreply.github.com>
2018-10-23 11:33:54 -0600

    Update and rename BMI_calculator.h to person.h
    Created the rest of the necessary functions.

--- person.h
+++ person.h
@@ -1,10 +1,11 @@
-#ifndef BMI_CALCULATOR_H
-#define BMI_CALCULATOR_H
+#ifndef PERSON_H
+#define PERSON_H
 
 class Person
 {
 	//This class stores the information of a person that wants to keep track of their BMI
 public:
+	Person();
 	Person(string name, int age, double height, double weight);
 	string getPersonName();
 	void setPersonName(string newName);
@@ -14,11 +15,16 @@ public:
 	void setPersonHeight(double newHeight);
 	double getPersonWeight();
 	void setPersonWeight(double newWeight);
+	double getBmi();
+	void savePerson(ofstream& outFS);
+	void printData();
+	void printInfo();
 private:
 	string name;
 	int age;
 	double height;
 	double weight;
+	double bmi;
 };
 
 #endif 
